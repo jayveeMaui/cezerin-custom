c0825983bf831ae6149160d94cc0bf6800b613dc
DESKTOP-D2JD332\jayvee <jayvee.maui@gmail.com>
2018-07-28 17:57:26 +0800

    ~modif procfile

--- config/server.js
+++ config/server.js
@@ -18,7 +18,7 @@ module.exports = {
 	ajaxBaseUrl: `http://localhost:3001/ajax`,
 
 	// Access-Control-Allow-Origin
-	storeBaseUrl: `http://localhost:3000`,
+	storeBaseUrl: `https://shop-app-app.herokuapp.com/`,
 
 	// used by API
 	adminLoginUrl: '/admin/login',
